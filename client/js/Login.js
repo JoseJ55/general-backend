206a0f4c381c60b5124b5e1e73663736e998aac6
JoseJ55 <josejimenez826@gmail.com>
2023-03-09 23:16:56 -0800

    added the logic to the login screen

--- client/js/Login.js
+++ client/js/Login.js
@@ -1,3 +1,24 @@
+// Field and button for the form to get user info to login
+const usernameField = document.getElementById('login-box-form-username');
+const passwordField = document.getElementById('login-box-form-password');
+const loginButton = document.getElementById('login-box-form-submit')
+const errorText = document.getElementById('login-box-form-error');
+
+// This function checks if the field are correct in order to login user
+function checkFields() {
+    if(usernameField.value.length == 0 || passwordField.value.length == 0) {
+        errorText.textContent = 'Fields can not be empty';
+        errorText.style.visibility = 'visible';
+    } else {
+        errorText.style.visibility = 'hidden';
+    }
+}
+
+loginButton.addEventListener('click', () => {
+    checkFields();
+})
+
+// Logic for going to the sign up screen
 const signInButton = document.getElementById('login-box-form-signup-button');
 
 signInButton.addEventListener('click', (e) => {
